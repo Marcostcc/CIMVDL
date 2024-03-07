0e325a3e5bfb4ef8b567dcd989eac825b530edf1
Marcos Torres Codeço Coelho <marcostorrescc@gmail.com>
2024-03-07 14:04:00 -0300

    improved dashboard and added new visualizations

--- dashboard.py
+++ dashboard.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import numpy as np
 import pandas as pd
 import duckdb as duck
 import geopandas as gpd
@@ -59,46 +60,57 @@ quem_dormiu_int_concelho['pais_origem'] = quem_dormiu_int_concelho['pais_origem'
 # A1. open geojson file
 world = read_gpkg('C:/Users/marco/Documents/MEGA/CIMVDL/CIM-VDL/geopackages/mundo_simples.gpkg', 'mundo_simples')
 
+# A1. remove row where NAME == Antarctica from world
+world = world[world['NAME'] != 'Antarctica']
+
 # A1. merge dos dados com o geodataframe
 world_gdf = world.merge(quem_dormiu_int_concelho, left_on='NAME_PT', right_on='pais_origem', how='left')
+
+# A2. Query para obter os dados dos paises que mais visitam
+with duck.connect('CIM_VDL.duckdb') as cursor:
+    total_dormidas_by_pais = pd.DataFrame(data=cursor.execute("select * from estadias_int_por_pais").fetchall(), columns=["pais_origem", "dormidas_totais"]).sort_values(by='dormidas_totais', ascending=False)
+
+# A2. Query para obter os dados dos paises por concelho
+with duck.connect('CIM_VDL.duckdb') as cursor:
+    total_dormidas_by_concelho = pd.DataFrame(data=cursor.execute("select * from estadias_int_per_concelho").fetchall(), columns=["pais_origem", "geo_area_nome", "dormidas_totais"]).sort_values(by='dormidas_totais', ascending=False)
 #-----------------------------------------------------------------------------#
 
-# A2. Query para obter os dados NACIONAIS
+# B1. Query para obter os dados NACIONAIS
 with duck.connect('CIM_VDL.duckdb') as cursor:
-    quem_dormiu_nac_concelho = pd.DataFrame(data=cursor.execute("select * from quem_dormiu_nac_concelho").fetchall(), columns=["pais_origem", "geo_area_nome", "concelho_residencia","dormidas_totais"])
+    quem_dormiu_nac_concelho = pd.DataFrame(data=cursor.execute("select * from quem_dormiu_nac_concelho").fetchall(), columns=["pais_origem", "geo_area_nome", "concelho_residencia","distrito_residencia","dormidas_totais"])
 
-# A2. geodataframe de PORTUGAL
+# B1. geodataframe de PORTUGAL
 portugal_con = read_gpkg('C:/Users/marco/Documents/MEGA/CIMVDL/CIM-VDL/geopackages/simplified_portugal.gpkg', 'simplified')
 
-# A2. merge dos dados com o geodataframe
+# B1. merge dos dados com o geodataframe
 portugal_gdf = portugal_con.merge(quem_dormiu_nac_concelho, left_on='Concelho', right_on='concelho_residencia', how='left')
 
 #-----------------------------------------------------------------------------#
 
-# A3. Query para obter os dados VISEU DÃO LAFÕES
+# C1. Query para obter os dados VISEU DÃO LAFÕES
 with duck.connect('CIM_VDL.duckdb') as cursor:
     quem_dormiu_reg_concelho = pd.DataFrame(data=cursor.execute("select * from quem_dormiu_reg_concelho").fetchall(), columns=["pais_origem", "geo_area_nome", "concelho_residencia","dormidas_totais"])
 
-# A3. geodataframe de VDL
+# C1. geodataframe de VDL
 vdl_con = read_gpkg('C:/Users/marco/Documents/MEGA/CIMVDL/CIM-VDL/geopackages/vdl_con.gpkg', 'vdl_con')
 
-# A3. merge dos dados com o geodataframe
+# C1. merge dos dados com o geodataframe
 vdl_gdf = vdl_con.merge(quem_dormiu_reg_concelho, left_on='Concelho', right_on='concelho_residencia', how='left')
 
 
 # criar cloropleth map com os dados
-a1, a2, a3 = st.columns((4,3,3))
+a1, a2 = st.columns((5.5,4.5))
 
 # with a1 plot the map for the world with the selected concelho
 with a1:
     # add discriptive
-    #st.write('Residentes Internacionais')
+    st.write('Quantidade de dormidas registadas por Turistas Internacionais em' + ' ' + select1)
     # Plot the geometry of the world with gray color
-    fig, ax = plt.subplots(figsize=(10, 6))
+    fig1, ax = plt.subplots(figsize=(10, 6))
     world_gdf.plot(ax=ax, color='lightgray')
 
     # Plot the geometry of the world with LogNorm color scale
-    world_gdf[world_gdf['geo_area_nome'] == select1].plot(column='dormidas_totais', cmap='viridis', legend=True, ax=ax, norm=LogNorm(vmin=1, vmax=20000))
+    world_gdf[world_gdf['geo_area_nome'] == select1].plot(column='dormidas_totais', cmap='viridis', legend=True, ax=ax, norm=LogNorm(vmin=1, vmax=20000),legend_kwds={'shrink': 0.3})
     #Add the names of distritos to portugal
     # for distrito in world_gdf['Distrito'].unique():
     #     x = world_gdf[world_gdf['Distrito'] == distrito].unary_union.centroid.x
@@ -106,21 +118,71 @@ with a1:
     #     ax.annotate(distrito, xy=(x, y), xytext=(3, 3), textcoords='offset points', fontsize=8, color='black', zorder=3, path_effects=[withStroke(linewidth=4, foreground='w')])
 
     # Set title and remove axis labels
-    plt.title('Visitantes Residentes Internacionais')
-    plt.gca().patch.set_alpha(0)
-    plt.axis('off')
-    st.pyplot(fig)
+    plt.title('Mapa de calor dos Turistas Internacionais')
+    ax.patch.set_alpha(0)
+    ax.axis('off')
+    st.pyplot(fig1)
 
 # with a2 plot the map for portugal with the selected concelho
 with a2:
+    bar_width = 0.3
+    st.write('Turistas Internacionais registados em' + ' ' + select1 + ' ' + 'vs Total em Viseu Dão Lafões')
+    # Get the top 10 countries from total_dormidas_by_pais
+    top_10_countries = total_dormidas_by_pais.head(10)['pais_origem']
+
+    r1 = np.arange(len(total_dormidas_by_pais.head(10)))
+
+    # Create an array of indices for the bars representing dormidas for Viseu
+    r2 = [x + bar_width for x in r1]
+
+    # Filter total_dormidas_by_concelho for the specific concelho Viseu and top 10 countries
+    filtered_dormidas_concelho = total_dormidas_by_concelho[(total_dormidas_by_concelho['geo_area_nome'] == select1) & (total_dormidas_by_concelho['pais_origem'].isin(top_10_countries))]
+
+    # Create figure and axis objects
+    figA2, ax = plt.subplots(figsize=(10, 6))
+
+    # Plot the bars
+    bars1 = ax.bar(r1, total_dormidas_by_pais.dormidas_totais.head(10), color='skyblue', width=bar_width, edgecolor='grey', label='Total de Estadias em VDL')
+    bars2 = ax.bar(r2, filtered_dormidas_concelho['dormidas_totais'], color='lightgreen', width=bar_width, edgecolor='grey', label='Estadias em' + ' ' + select1)
+
+    # Add labels, title, and legend
+    ax.set_xlabel('País de Origem', fontweight='bold')
+    ax.set_ylabel('Quantidade de Estadias', fontweight='bold')
+    ax.set_title('Comparação de Estadias em Viseu Dão Lafões e em'  + ' ' + select1, fontweight='bold')
+    ax.set_xticks([r + bar_width/2 for r in range(len(total_dormidas_by_pais.pais_origem.head(10)))])
+    ax.set_xticklabels(total_dormidas_by_pais.pais_origem.head(10))
+    ax.legend()
+
+    # Add value labels on top of the bars
+    for bars in [bars1, bars2]:
+        for bar in bars:
+            height = bar.get_height()
+            ax.annotate('{}'.format(height),
+                        xy=(bar.get_x() + bar.get_width() / 2, height),
+                        xytext=(0, 3),  # 3 points vertical offset
+                        textcoords="offset points",
+                        ha='center', va='bottom')
+
+    # Show plot
+    # Remove the border color of the figure
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=45)
+    st.pyplot(figA2)
+
+b1, b2 = st.columns((5.5,4.5))
+
+with b1:
     # add discriptive
-    #st.write('Residentes em Portugal')
+    st.write('Quantidade de dormidas registadas por Turistas Nacionais em' + ' ' + select1)
     # Plot the geometry of Portugal with gray color
-    fig, ax = plt.subplots(figsize=(10, 6))
+    fig2, ax = plt.subplots(figsize=(10, 6))
     portugal_gdf.plot(ax=ax, color='lightgray')
 
     # Plot the geometry of Viseu with LogNorm color scale
-    portugal_gdf[portugal_gdf['geo_area_nome'] == select1].plot(column='dormidas_totais', cmap='viridis', legend=True, ax=ax, norm=LogNorm(vmin=1, vmax=20000))
+    portugal_gdf[portugal_gdf['geo_area_nome'] == select1].plot(column='dormidas_totais', cmap='viridis', legend=True, ax=ax, norm=LogNorm(vmin=1, vmax=20000),legend_kwds={'shrink': 0.3})
     #Add the names of distritos to portugal
     # for distrito in portugal_gdf['Distrito'].unique():
     #     x = portugal_gdf[portugal_gdf['Distrito'] == distrito].unary_union.centroid.x
@@ -128,18 +190,20 @@ with a2:
     #     ax.annotate(distrito, xy=(x, y), xytext=(3, 3), textcoords='offset points', fontsize=8, color='black', zorder=3, path_effects=[withStroke(linewidth=4, foreground='w')])
 
     # Set title and remove axis labels
-    plt.title('Visitantes Residentes em Portugal')
-    plt.gca().patch.set_alpha(0)
-    plt.axis('off')
-    st.pyplot(fig)
+    plt.title('Mapa de calor dos Turistas Nacionais')
+    ax.patch.set_alpha(0)
+    ax.axis('off')
+    st.pyplot(fig2)
 
-with a3:
+c1, c2 = st.columns((5.5,4.5))
+
+with c1:
     # add discriptive
-    #st.write('Residentes em Viseu Dão Lafões')
+    st.write('Quantidade de dormidas registadas por Turistas de Viseu Dão Lafões em' + ' ' + select1)
     # Plot the geometry of Viseu with LogNorm color scale
-    fig, ax = plt.subplots(figsize=(10, 6))
+    fig3, ax = plt.subplots(figsize=(10, 6))
     vdl_gdf.plot(ax=ax, color='lightgray')
-    vdl_gdf[vdl_gdf['geo_area_nome'] == select1].plot(column='dormidas_totais', cmap='viridis', legend=True, ax=ax, norm=LogNorm(vmin=1, vmax=20000))
+    vdl_gdf[vdl_gdf['geo_area_nome'] == select1].plot(column='dormidas_totais', cmap='viridis', legend=True, ax=ax, norm=LogNorm(vmin=1, vmax=20000),legend_kwds={'shrink': 0.3})
     #Add the names of distritos to portugal
     # for distrito in vdl_gdf['Distrito'].unique():
     #     x = vdl_gdf[vdl_gdf['Distrito'] == distrito].unary_union.centroid.x
@@ -147,7 +211,7 @@ with a3:
     #     ax.annotate(distrito, xy=(x, y), xytext=(3, 3), textcoords='offset points', fontsize=8, color='black', zorder=3, path_effects=[withStroke(linewidth=4, foreground='w')])
 
     # Set title and remove axis labels
-    plt.title('Visitantes Residentes em Viseu Dão Lafões')
-    plt.gca().patch.set_alpha(0)
-    plt.axis('off')
-    st.pyplot(fig)
+    plt.title('Mapa de calor dos Turistas de Viseu Dão Lafões')
+    ax.patch.set_alpha(0)
+    ax.axis('off')
+    st.pyplot(fig3)
--- views.sql
+++ views.sql
@@ -2,9 +2,9 @@
 BEGIN TRANSACTION;
 
 -- create base views for deslocamento
-create or replace view deslocamento_national_concelho as select * from deslocamento_concelho_h where concelho_residencia IS NOT NULL AND NOT residente_VDL;
-create or replace view deslocamento_national_freguesia as select * from deslocamento_freguesia_h where concelho_residencia IS NOT NULL AND NOT residente_VDL;
-create or replace view deslocamento_national_seccao as select * from deslocamento_seccao_h where concelho_residencia IS NOT NULL AND NOT residente_VDL;
+create or replace view deslocamento_national_concelho as select * from deslocamento_concelho_h where pais_origem != 'Outros' AND concelho_residencia IS NOT NULL AND NOT residente_VDL;
+create or replace view deslocamento_national_freguesia as select * from deslocamento_freguesia_h where pais_origem != 'Outros' AND concelho_residencia IS NOT NULL AND NOT residente_VDL;
+create or replace view deslocamento_national_seccao as select * from deslocamento_seccao_h where pais_origem != 'Outros' AND concelho_residencia IS NOT NULL AND NOT residente_VDL;
 
 create or replace view deslocamento_regional_concelho as select * from deslocamento_concelho_h where residente_VDL AND geocodigo_residencia::text != substring(geocodigo_analisado::text, 1, length(geocodigo_analisado::text));
 create or replace view deslocamento_regional_freguesia as select * from deslocamento_freguesia_h where residente_VDL AND geocodigo_residencia::text != substring(geocodigo_analisado::text, 1, length(geocodigo_analisado::text) - 2);
@@ -22,9 +22,9 @@ create or replace view permanencia_internacional_concelho  as select * from perm
 create or replace view permanencia_internacional_freguesia as select * from permanencia_freguesia_d where pais_origem != 'Portugal' and concelho_residencia IS NULL;
 create or replace view permanencia_internacional_seccao as select * from permanencia_seccao_d where pais_origem != 'Portugal' and concelho_residencia IS NULL;
 
-create or replace view permanencia_nacional_concelho  as select * from permanencia_concelho_d where concelho_residencia IS NOT NULL AND NOT residente_VDL;
-create or replace view permanencia_nacional_freguesia as select * from permanencia_freguesia_d where concelho_residencia IS NOT NULL AND NOT residente_VDL;
-create or replace view permanencia_nacional_seccao as select * from permanencia_seccao_d where concelho_residencia IS NOT NULL AND NOT residente_VDL;
+create or replace view permanencia_nacional_concelho  as select * from permanencia_concelho_d where pais_origem != 'Outros' AND concelho_residencia IS NOT NULL AND NOT residente_VDL;
+create or replace view permanencia_nacional_freguesia as select * from permanencia_freguesia_d where pais_origem != 'Outros' AND concelho_residencia IS NOT NULL AND NOT residente_VDL;
+create or replace view permanencia_nacional_seccao as select * from permanencia_seccao_d where pais_origem != 'Outros' AND concelho_residencia IS NOT NULL AND NOT residente_VDL;
 
 create or replace view permanencia_regional_concelho  as select * from permanencia_concelho_d where residente_VDL AND concelho_residencia_codigo::text != substring(geo_area_codigo::text, 1, length(geo_area_codigo::text));
 create or replace view permanencia_regional_freguesia as select * from permanencia_freguesia_d where residente_VDL AND concelho_residencia_codigo::text != substring(geo_area_codigo::text, 1, length(geo_area_codigo::text) - 2);
@@ -40,9 +40,9 @@ create or replace view estadias_international_concelho  as select * from estadia
 create or replace view estadias_international_freguesia as select * from estadias_freguesia_m where pais_origem != 'Portugal' and concelho_residencia IS NULL;
 create or replace view estadias_international_seccao as select * from estadias_seccao_m where pais_origem != 'Portugal' and concelho_residencia IS NULL;
 
-create or replace view estadias_national_concelho  as select * from estadias_concelho_m where concelho_residencia IS NOT NULL AND NOT residente_VDL;
-create or replace view estadias_national_freguesia as select * from estadias_freguesia_m where concelho_residencia IS NOT NULL AND NOT residente_VDL;
-create or replace view estadias_national_seccao as select * from estadias_seccao_m where concelho_residencia IS NOT NULL AND NOT residente_VDL;
+create or replace view estadias_national_concelho  as select * from estadias_concelho_m where pais_origem != 'Outros' AND concelho_residencia IS NOT NULL AND NOT residente_VDL;
+create or replace view estadias_national_freguesia as select * from estadias_freguesia_m where pais_origem != 'Outros' AND concelho_residencia IS NOT NULL AND NOT residente_VDL;
+create or replace view estadias_national_seccao as select * from estadias_seccao_m where pais_origem != 'Outros' AND concelho_residencia IS NOT NULL AND NOT residente_VDL;
 
 create or replace view estadias_regional_concelho  as select * from estadias_concelho_m where residente_VDL AND concelho_residencia_codigo::text != substring(geo_area_codigo::text, 1, length(geo_area_codigo::text) );
 create or replace view estadias_regional_freguesia as select * from estadias_freguesia_m where residente_VDL AND concelho_residencia_codigo::text != substring(geo_area_codigo::text, 1, length(geo_area_codigo::text) - 2);
@@ -59,11 +59,16 @@ create or replace view estadias_local_seccao as select * from estadias_seccao_m 
 -- Quem dormiu?
 create or replace view quem_dormiu_int_concelho as select pais_origem, geo_area_nome, sum(dormidas_totais) as dormidas_totais from estadias_international_concelho group by pais_origem, geo_area_nome;
 create or replace view quem_dormiu_int_freguesia as select pais_origem, geo_area_nome,  sum(dormidas_totais) as dormidas_totais from estadias_international_freguesia group by pais_origem, geo_area_nome;
+create or replace view estadias_int_per_concelho as select pais_origem, geo_area_nome, SUM(dormidas_totais) AS dormidas_totais FROM quem_dormiu_int_concelho GROUP BY pais_origem, geo_area_nome ORDER BY dormidas_totais DESC;
+create or replace view estadias_int_por_pais as select pais_origem, sum(dormidas_totais) as dormidas_totais from estadias_international_concelho group by pais_origem;
 
-create or replace view quem_dormiu_nac_concelho as select pais_origem, geo_area_nome, concelho_residencia, sum(dormidas_totais) from estadias_national_concelho group by pais_origem, geo_area_nome, concelho_residencia,;
-create or replace view quem_dormiu_nac_freguesia as select pais_origem, geo_area_nome, concelho_residencia, sum(dormidas_totais) from estadias_national_freguesia group by pais_origem, geo_area_nome, concelho_residencia,;
+create or replace view quem_dormiu_nac_concelho as select pais_origem, geo_area_nome, concelho_residencia, distrito_residencia, sum(dormidas_totais) as dormidas_totais from estadias_national_concelho group by pais_origem, geo_area_nome, concelho_residencia, distrito_residencia;
+create or replace view quem_dormiu_nac_freguesia as select pais_origem, geo_area_nome, concelho_residencia, distrito_residencia, sum(dormidas_totais) as dormidas_totais from estadias_national_freguesia group by pais_origem, geo_area_nome, concelho_residencia, distrito_residencia;
+create or replace view estadias_nac_per_concelho as select pais_origem, geo_area_nome, SUM(dormidas_totais) AS dormidas_totais FROM quem_dormiu_nac_concelho GROUP BY pais_origem, geo_area_nome ORDER BY dormidas_totais DESC;
+create or replace view estadias_nac_por_distrito as select distrito_residencia, sum(dormidas_totais) as dormidas_totais from estadias_national_concelho group by distrito_residencia;
 
-create or replace view quem_dormiu_reg_concelho as select pais_origem, geo_area_nome, concelho_residencia, sum(dormidas_totais) from estadias_regional_concelho group by pais_origem, geo_area_nome, concelho_residencia;
-create or replace view quem_dormiu_reg_freguesia as select pais_origem, geo_area_nome, concelho_residencia, sum(dormidas_totais) from estadias_regional_freguesia group by pais_origem, geo_area_nome, concelho_residencia;
+create or replace view quem_dormiu_reg_concelho as select pais_origem, geo_area_nome, concelho_residencia, sum(dormidas_totais) as dormidas_totais from estadias_regional_concelho group by pais_origem, geo_area_nome, concelho_residencia;
+create or replace view quem_dormiu_reg_freguesia as select pais_origem, geo_area_nome, concelho_residencia, sum(dormidas_totais) as dormidas_totaiss from estadias_regional_freguesia group by pais_origem, geo_area_nome, concelho_residencia;
+--create or replace view estadias_reg_per_concelho  as select pais_origem, geo_area_nome, SUM(dormidas_totais) AS dormidas_totais FROM quem_dormiu_reg_concelho GROUP BY pais_origem, geo_area_nome ORDER BY dormidas_totais DESC;
 
 COMMIT;
